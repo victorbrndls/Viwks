c1aa559ef1eb63ce4b1c593216fafd3b43a1e69f
Harystolho <haristolhogamer@gmail.com>
2018-06-16 16:17:19 -0300

    Added new fonts Modified the gui

--- src/main/java/com/harystolho/controllers/MainController.java
+++ src/main/java/com/harystolho/controllers/MainController.java
@@ -10,22 +10,23 @@ import com.harystolho.task.TaskUtils;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
 
 public class MainController implements Controller {
 
 	@FXML
-	private Button openTask;
+	private Pane pane;
+
+	@FXML
+	private Button openTaskButton;
 
 	@FXML
 	private ListView<Task> taskList;
-
-	@FXML
-	private Text taskNameField;
 
 	@FXML
 	private Button runButton;
@@ -34,21 +35,27 @@ public class MainController implements Controller {
 	private Button deleteButton;
 
 	@FXML
-	private Button editButton;
+	private Button saveButton;
 
 	@FXML
 	private Text intervalField;
 
 	@FXML
-	private Text unitField;
-
-	@FXML
 	private TextField folderField;
-	private File outputFolder;
 
 	@FXML
 	private Button changeFolderButton;
 
+	@FXML
+	private Button editButton;
+
+	@FXML
+	private Label taskNameField;
+
+	@FXML
+	private Label unitField;
+
+	private File outputFolder;
 	private Task currentTask;
 
 	@FXML
@@ -62,7 +69,7 @@ public class MainController implements Controller {
 
 	private void loadEventListeners() {
 
-		openTask.setOnMouseClicked((e) -> {
+		openTaskButton.setOnMouseClicked((e) -> {
 			openTaskWindow();
 		});
 
